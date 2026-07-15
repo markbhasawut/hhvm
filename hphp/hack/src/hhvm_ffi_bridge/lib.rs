e91118e70388d111d039906686c0c053aaf7fd4f
Bhasawut Singhaphan <bhasawut@gmail.com>
2026-07-15 22:51:30 +0700

    cargo: add unified hhvm_ffi_bridge crate to workspace
    Adds a static, in-tree hhvm_ffi_bridge crate under hack/src/hhvm_ffi_bridge
    to represent the unified HHVM FFI staticlib rollup. Bundles compiler_ffi, hdf,
    hhvm_hhbc_defs_ffi, hhvm_types_ffi, packages_ffi, and parser_ffi dependencies.
    Also registers the crate in the workspace members list in Cargo.toml.

---
+++ hphp/hack/src/hhvm_ffi_bridge/lib.rs
@@ -0,0 +1,9 @@
+#![allow(unused_extern_crates)]
+
+// cxx expects a single Rust staticlib when the final link is done by CMake.
+extern crate compiler_ffi;
+extern crate hdf;
+extern crate hhvm_hhbc_defs_ffi;
+extern crate hhvm_types_ffi;
+extern crate packages_ffi;
+extern crate parser_ffi;
